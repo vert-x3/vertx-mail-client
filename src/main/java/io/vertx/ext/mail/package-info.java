7531cccd10020cb41b807cf7483e572636036848
alexlehm <alexlehm@gmail.com>
2014-12-18 22:23:27 +0100

    small asciidoc example in package-info.java asciidoc creation now works at least
    Signed-off-by: alexlehm <alexlehm@gmail.com>

--- src/main/java/io/vertx/ext/mail/package-info.java
+++ src/main/java/io/vertx/ext/mail/package-info.java
@@ -1,6 +1,53 @@
-/**
- * @author <a href="http://oss.lehmann.cx/">Alexander Lehmann</a>
+/*
+ * Copyright (c) 2011-2014 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
  */
+/**
+ * = Vert.x Mail (SMTP client implementation)
+ *
+ * The link:javadoc/io/vertx/groovy/ext/apex/MailService.html[`MailService`] is really cool stuff, here is an example
+ *
+ *
+ * [source,java]
+ * ----
+ * import io.vertx.ext.mail.MailConfig;
+ * import io.vertx.ext.mail.MailService;
+ * 
+ * MailConfig mailConfig = ServerConfigs.configSendgrid();
+ * mailConfig.setUsername(username);
+ * mailConfig.setPassword(password);
+ *
+ * MailService mailService = MailService.create(vertx, mailConfig);
+ *
+ * JsonObject email = new JsonObject();
+ * email.put("from", "address@example.com");
+ * email.put("recipient", "address@example.com");
+ * email.put("subject", "meaningful subject");
+ * email.put("text", "this is a message");
+ *
+ * mailService.sendMail(email, result -> {
+ *   if(result.succeeded()) {
+ *     log.info(result.result().toString());
+ *   } else {
+ *     log.warn("got exception", result.cause());
+ *   }
+ *
+ * });
+ * ----
+ */
+@Document(fileName = "index.adoc")
 @GenModule(name = "ext-mail")
 package io.vertx.ext.mail;
 import io.vertx.codegen.annotations.GenModule;
+import io.vertx.docgen.Document;
