725d957219d30c1a52fa386507756e85023f466f
alexlehm <alexlehm@gmail.com>
2015-02-17 19:17:12 +0100

    fix error in pom.xml change example to set hostname improved js test
    Signed-off-by: alexlehm <alexlehm@gmail.com>

--- src/test/resources/javascript/mailtest.js
+++ src/test/resources/javascript/mailtest.js
@@ -1,4 +1,3 @@
-
 //simple example to show that the generated interface works in javascript
 
 var Vertx = require("vertx-js/vertx");
@@ -6,42 +5,39 @@ var vertx = Vertx.vertx();
 
 var MailService = require('vertx-mail-js/mail_service');
 
-var config={
-    "hostname": "mail.arcor.de",
-    port: 587,
-    "username": "xxx",
-    "password": "xxx",
-    "starttls": "required"
+var config = {
+  "hostname" : "mail.arcor.de",
+  port : 587,
+  "username" : "xxx",
+  "password" : "xxx",
+  "starttls" : "required"
 };
 
 console.log("starting");
 
-var service=MailService.create(vertx,config);
+var service = MailService.create(vertx, config);
 
-var email={
-    "from": "user@exmample.com",
-    "to": "user@exmample.com",
-    "subject": "Test email",
-    "text": "this is a mail message",
-    "attachment":
-      [
-       {
-         "data": "attachment file content",
-         "content-type": "text/plain"
-       },
-       {
-         "data": "\0\0\0\0"
-//         "content-type": "application/octet-stream"
-       }
-       ]
+var email = {
+  "from" : "user@exmample.com",
+  "to" : "user@exmample.com",
+  "subject" : "Test email",
+  "text" : "this is a mail message",
+  "attachment" : [ {
+    "data" : "attachment file content",
+    "content-type" : "text/plain"
+  }, {
+    "data" : "\0\0\0\0"
+  // "content-type": "application/octet-stream"
+  } ]
 };
 
-service.sendMail(email, function(result) {
+service.sendMail(email, function (result, result_err) {
   console.log('Mail finished');
-  if(result != null) {
-    console.log('result: '+JSON.stringify(result));
+  if (result != null) {
+    console.log('result: ' + JSON.stringify(result));
   } else {
     console.log('sending failed');
+    result_err.printStackTrace();
   }
 });
 
