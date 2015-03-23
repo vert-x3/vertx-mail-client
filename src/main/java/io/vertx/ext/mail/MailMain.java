05089a9fa245a3459a989bff791dc70706acb3b1
alexlehm <alexlehm@gmail.com>
2015-03-23 21:52:28 +0100

    Refactored SMTPConnection into its own class changed all command methods to use String instead of AsyncResult<String> simplified the throwAsyncResult method a bit for SMTPConnection MAIL FROM and RCPT TO used the realname part of the email
    improved a test with Wiser
    
    Signed-off-by: alexlehm <alexlehm@gmail.com>

--- src/main/java/io/vertx/ext/mail/MailMain.java
+++ src/main/java/io/vertx/ext/mail/MailMain.java
@@ -4,13 +4,9 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.impl.LoggerFactory;
-import io.vertx.core.net.NetClient;
-import io.vertx.core.net.NetClientOptions;
-import io.vertx.core.net.NetSocket;
 import io.vertx.ext.mail.mailencoder.EmailAddress;
 import io.vertx.ext.mail.mailencoder.MailEncoder;
 
@@ -32,7 +28,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 /*
  * main operation of the smtp client
- * this class takes care of the different smtp steps, AUTH etc
+ *
+ * this class takes care of the different SMTP steps, AUTH etc
  * and generates the mail text by using the Email object
  */
 
@@ -46,19 +43,14 @@ class MailMain {
   private Handler<AsyncResult<JsonObject>> finishedHandler;
   private MailConfig config;
   private static final Logger log = LoggerFactory.getLogger(MailMain.class);
-  private NetSocket ns;
-  private boolean socketClosed;
-  private boolean socketShutDown;
-
-  private Handler<AsyncResult<String>> commandResultHandler;
 
   private MailMessage email;
   private String mailMessage;
-  private NetClient client;
 
   private Set<String> capaAuth = Collections.emptySet();
   private boolean capaStartTLS = false;
-  private int capaSize = 0;
+  private int capaSize;
+  private SMTPConnection connection = null;
 
   // 8BITMIME can be used if the server supports it, currently this is not
   // implemented
@@ -70,55 +62,6 @@ class MailMain {
     this.vertx = vertx;
     this.config = config;
     this.finishedHandler = finishedHandler;
-  }
-
-  private void shutdown() {
-    commandResultHandler = null;
-    socketShutDown = true;
-    if (ns != null) {
-      ns.close();
-      ns = null;
-    }
-    if (client != null) {
-      client.close();
-      client = null;
-    }
-  }
-
-  /*
-   * write command without masking anything
-   */
-  private void write(String str, Handler<AsyncResult<String>> commandResultHandler) {
-    write(str, -1, commandResultHandler);
-  }
-
-  /*
-   * write command masking everything after position blank
-   */
-  private void write(String str, int blank, Handler<AsyncResult<String>> commandResultHandler) {
-    this.commandResultHandler = commandResultHandler;
-    if (socketClosed) {
-      throwAsyncResult("connection was closed by server");
-    }
-    if (log.isDebugEnabled()) {
-      String logStr;
-      if (blank >= 0) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = blank; i < str.length(); i++) {
-          sb.append('*');
-        }
-        logStr = str.substring(0, blank + 1) + sb;
-      } else {
-        logStr = str;
-      }
-      // avoid logging large mail body
-      if (logStr.length() < 1000) {
-        log.debug("command: " + logStr);
-      } else {
-        log.debug("command: " + logStr.substring(0, 1000) + "...");
-      }
-    }
-    ns.write(str + "\r\n");
   }
 
   /**
@@ -150,57 +93,21 @@ class MailMain {
   private void sendMail() {
     // do a bit of validation before we open the connection
     validateHeaders();
-
-    NetClientOptions netClientOptions = new NetClientOptions().setSsl(config.isSsl());
-    client = vertx.createNetClient(netClientOptions);
-
-    client.connect(config.getPort(), config.getHostname(), asyncResult -> {
-      if (asyncResult.succeeded()) {
-        ns = asyncResult.result();
-        socketClosed = false;
-        ns.exceptionHandler(e -> {
-          // avoid returning two exceptions
-          if (!socketClosed && !socketShutDown) {
-            log.debug("got an exception on the netsocket", e);
-            throwAsyncResult(e);
-          }
-        });
-        ns.closeHandler(v -> {
-          // avoid exception if we regularly shut down the socket on our side
-          if (!socketShutDown) {
-            log.debug("socket has been closed");
-            socketClosed = true;
-            throwAsyncResult("connection has been closed by the server");
-          }
-        });
-        commandResultHandler = (result -> serverGreeting(result));
-        final Handler<Buffer> mlp = new MultilineParser(buffer -> {
-          if (commandResultHandler == null) {
-            log.debug("dropping reply arriving after we stopped processing \"" + buffer.toString() + "\"");
-          } else {
-            commandResultHandler.handle(Future.succeededFuture(buffer.toString()));
-          }
-        });
-        ns.handler(mlp);
-      } else {
-        log.error("exception on connect", asyncResult.cause());
-        throwAsyncResult(asyncResult.cause());
-      }
-    });
+    connection = new SMTPConnection();
+    connection.initializeConnection(vertx, config, this::serverGreeting, this::throwError);
   }
 
   private void validateHeaders() {
     if (email.getBounceAddress() == null && email.getFrom() == null) {
-      throwAsyncResult("sender address is not present");
+      throwError("sender address is not present");
     } else if ((email.getTo() == null || email.getTo().size() == 0)
         && (email.getCc() == null || email.getCc().size() == 0)
         && (email.getBcc() == null || email.getBcc().size() == 0)) {
-      throwAsyncResult("no recipient addresses are present");
+      throwError("no recipient addresses are present");
     }
   }
 
-  private void serverGreeting(AsyncResult<String> result) {
-    String message = result.result();
+  private void serverGreeting(final String message) {
     log.debug("server greeting: " + message);
     if (isStatusOk(message)) {
       if (isEsmtpSupported(message)) {
@@ -209,7 +116,7 @@ class MailMain {
         heloCmd();
       }
     } else {
-      throwAsyncResult("got error response " + message);
+      throwError("got error response " + message);
     }
   }
 
@@ -246,53 +153,56 @@ class MailMain {
   }
 
   private void ehloCmd() {
-    write("EHLO " + getMyHostname(), result -> {
-      String message = result.result();
-      log.debug("EHLO result: " + message);
-      if (isStatusOk(message)) {
-        setCapabilities(message);
+    connection
+        .write(
+            "EHLO " + getMyHostname(),
+            message -> {
+              log.debug("EHLO result: " + message);
+              if (isStatusOk(message)) {
+                setCapabilities(message);
 
-        // fail if we are exceeding size as early as possible
-        createMailMessage();
-        if (capaSize > 0 && mailMessage.length() > capaSize) {
-          throwAsyncResult("message exceeds allowed size limit");
-        } else {
-          if (capaStartTLS && !ns.isSsl()
-              && (config.getStarttls() == StarttlsOption.REQUIRED || config.getStarttls() == StarttlsOption.OPTIONAL)) {
-            // do not start TLS if we are connected with SSL
-            // or are already in TLS
-            startTLSCmd();
-          } else {
-            if (!ns.isSsl() && config.getStarttls() == StarttlsOption.REQUIRED) {
-              log.warn("STARTTLS required but not supported by server");
-              throwAsyncResult("STARTTLS required but not supported by server");
-            } else {
-              if (config.getLogin() != LoginOption.DISABLED && config.getUsername() != null && config.getPassword() != null
-                  && !capaAuth.isEmpty()) {
-                authCmd();
-              } else {
-                if (config.getLogin() == LoginOption.REQUIRED) {
-                  if (config.getUsername() != null && config.getPassword() != null) {
-                    throwAsyncResult("login is required, but no AUTH methods available. You may need to do STARTTLS");
-                  } else {
-                    throwAsyncResult("login is required, but no credentials supplied");
-                  }
+                // fail if we are exceeding size as early as possible
+                createMailMessage();
+                if (capaSize > 0 && mailMessage.length() > capaSize) {
+                  throwError("message exceeds allowed size limit");
                 } else {
-                  mailFromCmd();
+                  if (capaStartTLS
+                      && !connection.isSsl()
+                      && (config.getStarttls() == StarttlsOption.REQUIRED || config.getStarttls() == StarttlsOption.OPTIONAL)) {
+                    // do not start TLS if we are connected with SSL
+                    // or are already in TLS
+                    startTLSCmd();
+                  } else {
+                    if (!connection.isSsl() && config.getStarttls() == StarttlsOption.REQUIRED) {
+                      log.warn("STARTTLS required but not supported by server");
+                      throwError("STARTTLS required but not supported by server");
+                    } else {
+                      if (config.getLogin() != LoginOption.DISABLED && config.getUsername() != null
+                          && config.getPassword() != null && !capaAuth.isEmpty()) {
+                        authCmd();
+                      } else {
+                        if (config.getLogin() == LoginOption.REQUIRED) {
+                          if (capaAuth.isEmpty()) {
+                            throwError("login is required, but no AUTH methods available. You may need to do STARTTLS");
+                          } else {
+                            throwError("login is required, but no credentials supplied");
+                          }
+                        } else {
+                          mailFromCmd();
+                        }
+                      }
+                    }
+                  }
+                }
+              } else {
+                // if EHLO fails, assume we have to do HELO
+                if (isStatusTemporary(message)) {
+                  heloCmd();
+                } else {
+                  throwError("EHLO failed with " + message);
                 }
               }
-            }
-          }
-        }
-      } else {
-        // if EHLO fails, assume we have to do HELO
-        if (isStatusTemporary(message)) {
-          heloCmd();
-        } else {
-          throwAsyncResult("EHLO failed with " + message);
-        }
-      }
-    });
+            });
   }
 
   /**
@@ -333,8 +243,7 @@ class MailMain {
   }
 
   private void heloCmd() {
-    write("HELO " + getMyHostname(), result -> {
-      String message = result.result();
+    connection.write("HELO " + getMyHostname(), message -> {
       log.debug("HELO result: " + message);
       mailFromCmd();
     });
@@ -348,7 +257,8 @@ class MailMain {
       InetAddress ip = InetAddress.getLocalHost();
       return ip.getCanonicalHostName();
     } catch (UnknownHostException e) {
-      return "unknown";
+      // as a last resort, use localhost
+      return "localhost";
     }
   }
 
@@ -356,20 +266,15 @@ class MailMain {
    * 
    */
   private void startTLSCmd() {
-    write("STARTTLS", result -> {
-      String message = result.result();
+    connection.write("STARTTLS", message -> {
       log.debug("STARTTLS result: " + message);
-      upgradeTLS();
-    });
-  }
-
-  private void upgradeTLS() {
-    ns.upgradeToSsl(v -> {
-      log.debug("ssl started");
-      // capabilities may have changed, e.g.
-      // if a service only announces PLAIN/LOGIN
-      // on secure channel
-      ehloCmd();
+      connection.upgradeToSsl(v -> {
+        log.debug("ssl started");
+        // capabilities may have changed, e.g.
+        // if a service only announces PLAIN/LOGIN
+        // on secure channel (e.g. googlemail)
+          ehloCmd();
+        });
     });
   }
 
@@ -382,7 +287,7 @@ class MailMain {
     for (String l : splitByChar(message, '\n')) {
       if (!l.startsWith(resultCode) || l.charAt(3) != '-' && l.charAt(3) != ' ') {
         log.error("format error in multiline response");
-        throwAsyncResult("format error in multiline response");
+        throwError("format error in multiline response");
       } else {
         v.add(l.substring(4));
       }
@@ -414,32 +319,29 @@ class MailMain {
 
   private void authCmd() {
     if (capaAuth.contains("CRAM-MD5")) {
-      write("AUTH CRAM-MD5", result -> {
-        String message = result.result();
+      connection.write("AUTH CRAM-MD5", message -> {
         log.debug("AUTH result: " + message);
         cramMD5Step1(message.substring(4));
       });
     } else if (capaAuth.contains("PLAIN")) {
       String authdata = base64("\0" + config.getUsername() + "\0" + config.getPassword());
-      write("AUTH PLAIN " + authdata, 10, result -> {
-        String message = result.result();
+      connection.write("AUTH PLAIN " + authdata, 10, message -> {
         log.debug("AUTH result: " + message);
         if (!isStatusOk(message)) {
           log.warn("authentication failed");
-          throwAsyncResult("authentication failed");
+          throwError("authentication failed");
         } else {
           mailFromCmd();
         }
       });
     } else if (capaAuth.contains("LOGIN")) {
-      write("AUTH LOGIN", result -> {
-        String message = result.result();
+      connection.write("AUTH LOGIN", message -> {
         log.debug("AUTH result: " + message);
         sendUsername();
       });
     } else {
       log.warn("cannot find supported auth method");
-      throwAsyncResult("cannot find supported auth method");
+      throwError("cannot find supported auth method");
     }
   }
 
@@ -447,8 +349,7 @@ class MailMain {
     String message = decodeb64(string);
     log.debug("message " + message);
     String reply = hmacMD5hex(message, config.getPassword());
-    write(base64(config.getUsername() + " " + reply), 0, result -> {
-      String message2 = result.result();
+    connection.write(base64(config.getUsername() + " " + reply), 0, message2 -> {
       log.debug("AUTH step 2 result: " + message2);
       cramMD5Step2(message2);
     });
@@ -487,52 +388,51 @@ class MailMain {
       mailFromCmd();
     } else {
       log.warn("authentication failed");
-      throwAsyncResult("authentication failed");
+      throwError("authentication failed");
     }
   }
 
   private void sendUsername() {
-    write(base64(config.getUsername()), 0, result -> {
-      String message = result.result();
+    connection.write(base64(config.getUsername()), 0, message -> {
       log.debug("username result: " + message);
       sendPw();
     });
   }
 
   private void sendPw() {
-    write(base64(config.getPassword()), 0, result -> {
-      String message = result.result();
+    connection.write(base64(config.getPassword()), 0, message -> {
       log.debug("pw result: " + message);
       if (isStatusOk(message)) {
         mailFromCmd();
       } else {
         log.warn("authentication failed");
-        throwAsyncResult("authentication failed");
+        throwError("authentication failed");
       }
     });
   }
 
   private void mailFromCmd() {
     try {
-      String fromAddr = email.getFrom();
+      String fromAddr;
       String bounceAddr = email.getBounceAddress();
       if (bounceAddr != null && !bounceAddr.isEmpty()) {
         fromAddr = bounceAddr;
+      } else {
+        fromAddr = email.getFrom();
       }
-      new EmailAddress(fromAddr);
-      write("MAIL FROM:<" + fromAddr + ">", result -> {
-        String message = result.result();
+      EmailAddress from = new EmailAddress(fromAddr);
+      connection.write("MAIL FROM:<" + from.getEmail() + ">", message -> {
         log.debug("MAIL FROM result: " + message);
         if (isStatusOk(message)) {
           rcptToCmd();
         } else {
           log.warn("sender address not accepted: " + message);
-          throwAsyncResult("sender address not accepted: " + message);
+          throwError("sender address not accepted: " + message);
         }
       });
     } catch (IllegalArgumentException e) {
       log.error("address exception", e);
-      throwAsyncResult(e);
+      throwError(e);
     }
   }
 
@@ -552,10 +452,8 @@ class MailMain {
 
   private void rcptToCmd(List<String> recipientAddrs, int i) {
     try {
-      String toAddr = recipientAddrs.get(i);
-      new EmailAddress(toAddr);
-      write("RCPT TO:<" + toAddr + ">", result -> {
-        String message = result.result();
+      EmailAddress toAddr = new EmailAddress(recipientAddrs.get(i));
+      connection.write("RCPT TO:<" + toAddr.getEmail() + ">", message -> {
         log.debug("RCPT TO result: " + message);
         if (isStatusOk(message)) {
           if (i + 1 < recipientAddrs.size()) {
@@ -565,42 +463,42 @@ class MailMain {
           }
         } else {
           log.warn("recipient address not accepted: " + message);
-          throwAsyncResult("recipient address not accepted: " + message);
+          throwError("recipient address not accepted: " + message);
         }
       });
     } catch (IllegalArgumentException e) {
       log.error("address exception", e);
-      throwAsyncResult(e);
+      throwError(e);
     }
   }
 
   private void dataCmd() {
-    write("DATA", result -> {
-      String message = result.result();
+    connection.write("DATA", message -> {
       log.debug("DATA result: " + message);
       if (isStatusOk(message)) {
         sendMaildata();
       } else {
         log.warn("DATA command not accepted: " + message);
-        throwAsyncResult("DATA command not accepted: " + message);
+        throwError("DATA command not accepted: " + message);
       }
     });
   }
 
   private void sendMaildata() {
-    // make sure we create the message here if it hasn't been created
+    // create the message here if it hasn't been created
     // for the size check above
     createMailMessage();
     // convert message to escape . at the start of line
     // TODO: this is probably bad for large messages
-    write(mailMessage.replaceAll("\n\\.", "\n..") + "\r\n.", result -> {
-      String message = result.result();
+    // TODO: it is probably required to convert \n to \r\n to be completely
+    // SMTP compliant
+    connection.write(mailMessage.replaceAll("\n\\.", "\n..") + "\r\n.", message -> {
       log.debug("maildata result: " + message);
       if (isStatusOk(message)) {
         quitCmd();
       } else {
         log.warn("sending data failed: " + message);
-        throwAsyncResult("sending data failed: " + message);
+        throwError("sending data failed: " + message);
       }
     });
   }
@@ -616,33 +514,36 @@ class MailMain {
   }
 
   private void quitCmd() {
-    write("QUIT", result -> {
-      String message = result.result();
+    connection.write("QUIT", message -> {
       log.debug("QUIT result: " + message);
       if (isStatusOk(message)) {
         finishMail();
       } else {
         log.warn("quit failed: " + message);
-        throwAsyncResult("quit failed: " + message);
+        throwError("quit failed: " + message);
       }
     });
   }
 
   private void finishMail() {
-    shutdown();
     JsonObject result = new JsonObject();
     result.put("result", "success");
-    finishedHandler.handle(Future.succeededFuture(result));
+    returnResult(Future.succeededFuture(result));
   }
 
-  private void throwAsyncResult(Throwable throwable) {
-    shutdown();
-    finishedHandler.handle(Future.failedFuture(throwable));
+  private void throwError(Throwable throwable) {
+    returnResult(Future.failedFuture(throwable));
   }
 
-  private void throwAsyncResult(String message) {
-    shutdown();
-    finishedHandler.handle(Future.failedFuture(message));
+  private void throwError(String message) {
+    returnResult(Future.failedFuture(message));
+  }
+
+  private void returnResult(Future<JsonObject> result) {
+    if (connection != null) {
+      connection.shutdown();
+    }
+    finishedHandler.handle(result);
   }
 
   private String base64(String string) {
