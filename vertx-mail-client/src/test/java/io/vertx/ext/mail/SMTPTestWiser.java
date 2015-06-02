b4b5cd5f4a9c8a90645904c1818a45eee292039e
alexlehm <alexlehm@gmail.com>
2015-06-02 20:29:07 +0200

    fix issue #29 MailEncoderTest should check decoded text added checks based on MimeMessage to a few of the tests

--- vertx-mail-client/src/test/java/io/vertx/ext/mail/SMTPTestWiser.java
+++ vertx-mail-client/src/test/java/io/vertx/ext/mail/SMTPTestWiser.java
@@ -1,5 +1,13 @@
 package io.vertx.ext.mail;
 
+import static org.hamcrest.core.StringContains.containsString;
+
+import java.security.Security;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.mail.internet.MimeMessage;
+
 import org.junit.After;
 import org.junit.Before;
 import org.slf4j.LoggerFactory;
@@ -8,15 +16,6 @@ import org.subethamail.smtp.AuthenticationHandlerFactory;
 import org.subethamail.smtp.RejectException;
 import org.subethamail.wiser.Wiser;
 import org.subethamail.wiser.WiserMessage;
-
-import javax.mail.internet.MimeMessage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.Security;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.core.StringContains.containsString;
 
 /**
  * Start/stop a dummy test server for each test
@@ -74,15 +73,6 @@ public class SMTPTestWiser extends SMTPTestBase {
     }
   }
 
-  protected String inputStreamToString(final InputStream inputStream) throws IOException {
-    final StringBuilder string = new StringBuilder();
-    int ch;
-    while ((ch = inputStream.read()) != -1) {
-      string.append((char) ch);
-    }
-    return string.toString();
-  }
-
   protected AdditionalAsserts assertExampleMessage() {
     return () -> {
       final WiserMessage message = wiser.getMessages().get(0);
@@ -90,7 +80,7 @@ public class SMTPTestWiser extends SMTPTestBase {
       final MimeMessage mimeMessage = message.getMimeMessage();
       assertThat(mimeMessage.getContentType(), containsString("text/plain"));
       assertEquals("Subject", mimeMessage.getSubject());
-      assertEquals("Message\n", TestUtils.conv2nl(inputStreamToString(mimeMessage.getInputStream())));
+      assertEquals("Message\n", TestUtils.conv2nl(TestUtils.inputStreamToString(mimeMessage.getInputStream())));
     };
   }
 
