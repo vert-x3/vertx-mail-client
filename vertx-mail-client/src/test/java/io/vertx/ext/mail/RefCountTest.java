9e334e7698bd4f8b6b9f17a096d6ab88b67e3cc6
Clement Escoffier <clement.escoffier@gmail.com>
2015-09-09 09:26:29 +0200

    Update license header.

--- vertx-mail-client/src/test/java/io/vertx/ext/mail/RefCountTest.java
+++ vertx-mail-client/src/test/java/io/vertx/ext/mail/RefCountTest.java
@@ -1,15 +1,15 @@
 /*
- * Copyright 2014 Red Hat, Inc.
+ *  Copyright (c) 2011-2015 The original author or authors
  *
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  and Apache License v2.0 which accompanies this distribution.
  *
- *  The Eclipse Public License is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+ *       The Eclipse Public License is available at
+ *       http://www.eclipse.org/legal/epl-v10.html
  *
- *  The Apache License v2.0 is available at
- *  http://www.opensource.org/licenses/apache2.0.php
+ *       The Apache License v2.0 is available at
+ *       http://www.opensource.org/licenses/apache2.0.php
  *
  *  You may elect to redistribute this code under either of these licenses.
  */
