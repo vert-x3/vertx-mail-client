9a8a7d37937cf3bb0d5884a2a469f8c085586688
Paulo Lopes <pmlopes@gmail.com>
2018-06-13 14:56:07 +0200

    Add missing files
    (cherry picked from commit 113e50a6a84a2406d8507f0cc916acb3b2b592b4)

---
+++ src/main/java/io/vertx/ext/mail/impl/sasl/AuthXOAUTH2.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2011-2015 The original author or authors
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *       The Eclipse Public License is available at
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *       The Apache License v2.0 is available at
+ *       http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+
+/**
+ *
+ */
+package io.vertx.ext.mail.impl.sasl;
+
+/**
+ * @author <a href="http://oss.lehmann.cx/">Alexander Lehmann</a>
+ */
+class AuthXOAUTH2 extends AuthBaseClass {
+
+  private static final String AUTH_NAME = "XOAUTH2";
+  private boolean first;
+
+  /**
+   * @param username
+   * @param accessToken
+   */
+  public AuthXOAUTH2(String username, String accessToken) {
+    super(username, accessToken);
+    first = true;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see io.vertx.ext.mail.impl.AuthBaseClass#getName()
+   */
+  @Override
+  public String getName() {
+    return AUTH_NAME;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see io.vertx.ext.mail.impl.AuthBaseClass#nextStep(java.lang.String)
+   */
+  @Override
+  public String nextStep(String data) {
+    if (first) {
+      first = false;
+      return "user=" + username + "\1auth=Bearer " + password + "\1\1";
+    } else {
+      return null;
+    }
+  }
+}
